0c6c0513bfd74e2a396104f438ab4fec919f725d
amin99a <35037610+amin99a@users.noreply.github.com>
2025-08-16 15:36:25 +0100

    Create section_toggles.dart

---
+++ lib/debug/section_toggles.dart
@@ -0,0 +1,11 @@
+class CarDetailsVisibility {
+  // Flip these to false, one by one, to isolate layout/runtime issues
+  static const bool showImageSection = true;
+  static const bool showHostDetailsSection = true;
+  static const bool showDatePickupSection = true;
+  static const bool showCarDetailsSection = true;
+  static const bool showRequirementsSection = true;
+  static const bool showConfirmBar = true;
+}
+
+
